53d4307ce1eeb7a7257acb1f25f155b742bdca44
Mhzzzzz <45113275+Mhzzzzz@users.noreply.github.com>
2020-05-30 11:28:07 +0800

    Update run_textcnn.py

--- run_textcnn.py
+++ run_textcnn.py
@@ -104,7 +104,7 @@ def main():
 		if (epoch + 1) % SAVE_EVERY == 0:
 			print('saving parameters')
 			os.makedirs('models', exist_ok=True)
-			model.save(model.state_dict(), 'textcnn-' + str(epoch) + '.pkl')
+			model.save(model.state_dict(), 'model/textcnn-' + str(epoch) + '.pkl')
 
 
 if __name__ == '__main__':
