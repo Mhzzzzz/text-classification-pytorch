8704e66bbf99ac6fd42d2c1f8e4f319cbfa62784
Mhzzzzz <45113275+Mhzzzzz@users.noreply.github.com>
2020-05-30 11:25:28 +0800

    Update run_textcnn.py

--- run_textcnn.py
+++ run_textcnn.py
@@ -60,6 +60,7 @@ def main():
 	summary(model, (20,))
 	criteration = nn.CrossEntropyLoss()
 	optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE)
+	print()
 
 	# ======================
 	# 训练与测试
